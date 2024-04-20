e2eee809fdff0ee7dd9a9b2874830c6191862ae4
minhducctchv <sieunhanbay1997@gmail.com>
2024-04-20 21:04:50 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
 import { useSearchParams } from "next/navigation";
+//@ts-ignore
 import { Howl } from "howler";
 import { isMobile } from "react-device-detect";
 
